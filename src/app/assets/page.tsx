b12eeb91d3773330b3dfbc6d1be894f1319f2e33
tharsila <tharsilapaula@gmail.com>
2025-02-22 18:33:20 -0300

    feat: adiciona pagina com listagem de ativos

---
+++ src/app/assets/page.tsx
@@ -0,0 +1,15 @@
+// src/app/assets/page.tsx
+
+
+import { AssetList } from '@/components/asset/AssetList';
+
+
+export default function AssetsPage (){
+  return (
+    <main>
+      <h1>Lista de Ativos</h1>
+      <AssetList />
+    </main>
+  );
+};
+
