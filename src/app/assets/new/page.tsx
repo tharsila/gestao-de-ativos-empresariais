5d0661beefe6c15cea432dae0d029a38b6c1f150
tharsila <tharsilapaula@gmail.com>
2025-02-22 18:33:42 -0300

    feat: adiciona pagina para criação de ativo

---
+++ src/app/assets/new/page.tsx
@@ -0,0 +1,13 @@
+import AssetForm from "@/components/form/AssetForm";
+
+// src/app/assets/new/page.tsx
+const NewAssetPage = () => {
+  return (
+    <div>
+      <h1>Cadastrar Novo Ativo</h1>
+      <AssetForm />
+    </div>
+  );
+};
+
+export default NewAssetPage;
