b70a4fac0988017887834ca630decdba1f086474
tharsila <tharsilapaula@gmail.com>
2025-02-22 18:27:58 -0300

    feat: adiciona `asset services` com chamadas a api (CRUD)

---
+++ src/services/AssetServices.ts
@@ -0,0 +1,56 @@
+const API = process.env.NEXT_PUBLIC_API_URL;
+
+export const assetService = {
+  getAssets: async () => {
+    const response = await fetch(`${API}/assets`);
+    return response.json();
+  },
+
+  createAsset: async (data: any) => {
+    const res = await fetch(`${API}/assets`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!res.ok) throw new Error('Erro ao criar ativo');
+    return res.json();
+  },
+
+  getAssetById: async (id: string) => {
+    const res = await fetch(`${API}/assets/${id}`);
+    if (!res.ok) return null;
+    return res.json();
+  },
+
+  updateAsset: async (id: string, data: any) => {
+    const res = await fetch(`${API}/assets/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    if (!res.ok) throw new Error('Erro ao atualizar ativo');
+    return res.json();
+  },
+
+ 
+  removeAsset: async (id: string) => {
+    const res = await fetch(`${API}/assets/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (!res.ok) {
+      throw new Error('Erro ao remover ativo');
+    }
+  
+    if (res.status === 204) {
+      return { success: true };
+    }
+    return res.json();
+  },
+  
+  
+};
