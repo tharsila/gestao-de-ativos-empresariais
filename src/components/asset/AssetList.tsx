c8a20eb191e05a0c1ed6fb29377f67ef4188e3d4
tharsila <tharsilapaula@gmail.com>
2025-02-22 19:38:46 -0300

    feat: adiciona botões de ação para editar e remover ativo

--- src/components/asset/AssetList.tsx
+++ src/components/asset/AssetList.tsx
@@ -5,6 +5,7 @@ import { useAssets } from '@/hooks/useAssets';
 import { Button } from '../ui/Button';
 import { useRouter } from 'next/navigation';
 
+
 export const AssetList: React.FC = () => {
   const router = useRouter();
   const { data, isLoading, error } = useAssets();
@@ -16,12 +17,40 @@ export const AssetList: React.FC = () => {
     { key: 'name', label: 'Nome' },
     { key: 'category', label: 'Categoria' },
     { key: 'status', label: 'Status' },
+    { key: 'action', label: 'Ação' },
   ];
+
+  const handleEdit = (id: string) => {
+    router.push(`/assets/edit/${id}`);
+  };
+
+  const handleDelete = (id: string) => {
+    if (confirm('Tem certeza de que deseja excluir este ativo?')) {
+      console.log(`Ativo ${id} deletado`);
+    }
+  };
+
+  const actionColumnRenderer = (id: string) => (
+    <div>
+      <Button
+        variant='secondary'
+        onClick={() => handleEdit(id)}
+        style={{ marginRight: '10px' }}
+      >
+       Editar
+      </Button>
+      <Button variant='danger' onClick={() => handleDelete(id)}>
+        Remover
+      </Button>
+    </div>
+  );
 
   return (
     <>
-      <Button onClick={() => router.push('/assets/new')}>Cadastrar Ativo</Button>
-      <Table columns={columns} data={data} />
-    </> 
+      <Button onClick={() => router.push('/assets/new')}>
+        Cadastrar Ativo
+      </Button>
+      <Table columns={columns} data={data} renderActionColumn={actionColumnRenderer}/>
+    </>
   );
 };
