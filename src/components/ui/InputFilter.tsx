42915f528995bc714799fa4e018cdf368c84509a
tharsila <tharsilapaula@gmail.com>
2025-02-23 16:30:37 -0300

    feat: adiciona input filter

---
+++ src/components/ui/InputFilter.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { useFormContext } from 'react-hook-form';
+import { ChangeEvent } from 'react';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const StyledInput = styled.input`
+  padding: 8px;
+  font-size: 16px;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  transition: 0.2s;
+  width: 200px;
+
+  &:focus {
+    border-color: #007bff;
+    outline: none;
+  }
+`;
+
+export const InputFilter: React.FC<InputProps> = ({ name, value, onChange, ...rest }) => {
+  return (
+    <StyledInput id={name} name={name} value={value} onChange={onChange} {...rest}/>
+  );
+};
