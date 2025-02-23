e1cd57d3f12c6b86ba1865985166be68672202dc
tharsila <tharsilapaula@gmail.com>
2025-02-23 10:48:50 -0300

    feat: adiciona componente SelectFilter

---
+++ src/components/ui/SelectFilter.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import styled from 'styled-components';
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface SelectProps {
+  name: string;
+  options: Option[];
+  onChange?: () => void;
+}
+
+const StyledSelect = styled.select`
+  padding: 8px 12px;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  font-size: 14px;
+  width: 200px;
+  outline: none;
+  cursor: pointer;
+  background-color: #fff;
+  transition: border-color 0.3s;
+
+  &:focus {
+    border-color: #007bff;
+  }
+`;
+
+export const SelectFilter: React.FC<SelectProps> = ({
+  name,
+  onChange,
+  options,
+}) => {
+  return (
+    <StyledSelect id={name} name={name} onChange={onChange}>
+      <option value=''>Selecione...</option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+};
