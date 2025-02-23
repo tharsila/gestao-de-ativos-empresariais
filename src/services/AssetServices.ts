d2600b8849b7f3a00e3c5e790c5f478b353a7684
tharsila <tharsilapaula@gmail.com>
2025-02-23 10:47:22 -0300

    feat: adiciona suporte a filtros e paginação em getAssets

--- src/services/AssetServices.ts
+++ src/services/AssetServices.ts
@@ -1,8 +1,25 @@
 const API = process.env.NEXT_PUBLIC_API_URL;
 
 export const assetService = {
-  getAssets: async () => {
-    const response = await fetch(`${API}/assets`);
+  getAssets: async ({
+    search = '',
+    category = '',
+    status = '',
+    sortBy = '',
+    page = 1,
+    per_page = 1,
+  }) => {
+
+    const queryParams = new URLSearchParams();
+
+    if (search) queryParams.append('name_like', search);
+    if (category) queryParams.append('category', category);
+    if (status) queryParams.append('status', status);
+    if (sortBy) queryParams.append('sortBy', sortBy);
+    queryParams.append('page', page.toString());
+    queryParams.append('_limit', per_page.toString());
+
+    const response = await fetch(`${API}/assets?${queryParams.toString()}`);
     return response.json();
   },
 
@@ -35,22 +52,19 @@ export const assetService = {
     return res.json();
   },
 
- 
   removeAsset: async (id: string) => {
     const res = await fetch(`${API}/assets/${id}`, {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' },
     });
-  
+
     if (!res.ok) {
       throw new Error('Erro ao remover ativo');
     }
-  
+
     if (res.status === 204) {
       return { success: true };
     }
     return res.json();
   },
-  
-  
 };
