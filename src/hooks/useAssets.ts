660f0cefec6d4cfdf55cdde63b33bd09839fe602
tharsila <tharsilapaula@gmail.com>
2025-02-23 10:48:07 -0300

    feat: adiciona suporte a filtros e paginação no hook useAssets

--- src/hooks/useAssets.ts
+++ src/hooks/useAssets.ts
@@ -1,7 +1,17 @@
 import { assetService } from '@/services/AssetServices';
 import { useQuery } from '@tanstack/react-query';
 
-export const useAssets = () => {
-  return useQuery({queryKey: ['assets'], queryFn: assetService.getAssets});
+export const useAssets = (filters: {
+  search?: string;
+  category?: string;
+  status?: string;
+  sortby: string;
+  page: number;
+  per_page?: number;
+}) => {
+  return useQuery({
+    queryKey: ['assets', filters],
+    queryFn: () => assetService.getAssets(filters),
+    
+  });
 };
-
