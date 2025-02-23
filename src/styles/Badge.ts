4e298936d93ce13ccdbd39518e72f5f409d17ced
tharsila <tharsilapaula@gmail.com>
2025-02-23 18:04:53 -0300

    style: adiciona badge

---
+++ src/styles/Badge.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Badge = styled.span<{ status?: string }>`
+  display: inline-block;
+  padding: 5px 10px;
+  border-radius: 12px;
+  font-weight: bold;
+  text-transform: capitalize;
+
+  ${({ status }) =>
+    status === 'Em manutenção' &&
+    `
+    background-color: #ffcc00;
+    color: #000;
+  `}
+
+  ${({ status }) =>
+    status === 'Inativo' &&
+    `
+    background-color: #e74c3c;
+    color: #fff;
+  `}
+`;
